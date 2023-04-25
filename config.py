833a62faab3e562ed1c8c8af94864efb7c6fc189
Elena Bogatyreva <130645562+Smolenalena@users.noreply.github.com>
2023-04-25 14:38:15 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+community_token = ''
+access_token = 'vk1.a.peJ8-rBo40p54o3ShyOH1axwzmda9a8J8tJscYDRgFdkERrRvM9jhqO1UNY-9_ekTaxZ1YLPTJWLV4IEeEt1NBtk2mhZXNMf5ggG5psVeRPeMsU6OZzqnujAv-OfHEd63DZaRMRtPEdbbGps_Z11t3oK-TvHizkE4Audc3HDNaD3ka5ZCghb212wRS6CwZ10cJBIjQdfzxKMDwL94eTo9Q'
+db_url_object = "postgresql+psycopg2://student:student@localhost/student_diplom"
+user_id = '798289388'

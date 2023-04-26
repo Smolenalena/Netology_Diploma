7c806c600d796c43e922c2a4705433b26deb34c5
Elena Bogatyreva <130645562+Smolenalena@users.noreply.github.com>
2023-04-26 19:58:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-community_token = ''
+community_token = 'vk1.a.AdCv5pDSxPZ4m_kVo6le-AA1goBTUwTwhpL1bbmZUXAi1N8IPEKttyCcsVBbne0Chf41LMIHrZb-YuzaowZlHpp5VdGLSjYXFbcvcx8IMpI7lS5Vta1ThDmUS8N32cLfIhsj3NC8mfkBZChvsov5q-3nmMFxSB-K1_-S93vvAjoBRdB5SXhjayKxwhqmuETEVjy1D0v6r9TK0MFuhYGdVw'
 access_token = 'vk1.a.peJ8-rBo40p54o3ShyOH1axwzmda9a8J8tJscYDRgFdkERrRvM9jhqO1UNY-9_ekTaxZ1YLPTJWLV4IEeEt1NBtk2mhZXNMf5ggG5psVeRPeMsU6OZzqnujAv-OfHEd63DZaRMRtPEdbbGps_Z11t3oK-TvHizkE4Audc3HDNaD3ka5ZCghb212wRS6CwZ10cJBIjQdfzxKMDwL94eTo9Q'
 db_url_object = "postgresql+psycopg2://student:student@localhost/student_diplom"
 user_id = '798289388'
